c538be79e24f41be63281070d3afacd8ec214fa1
jonbaxt <jonbaxt@gmail.com>
2018-12-06 00:50:26 -0700

    Basic Skeleton Using BootStrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import NavApp from './Components/NavApp/NavApp';
+import TabsSection from './Components/TabsSection/TabsSection';
+import Articles from './Components/Articles/Articles';
 import './App.css';
 
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className='HeaderArea'>
+          <h4>Bloggy the Blogger</h4>
+
         </header>
+        <NavApp />
+        {/* <h1>Blogging App</h1> */}
+        <div className='BlogBody' >
+          <TabsSection />
+
+
+
+          {/* <Articles /> */}
+
+          <div className='RightSideBlogBody'>
+            <h4>Blog Body</h4>
+          </div>
+
+        </div>
+
       </div>
     );
   }
---
+++ src/Components/Articles/Articles.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Articles extends Component {
+
+    render() {
+
+        return (
+            <div className='ArticlesMain'>
+                <h3>Articles Area</h3>
+            </div>
+        )
+    }
+}
